919d9f059f6b5f9f00b8b84d0a8d8c1a2062042e
vnn1489 <vnn1489@outlook.com>
2023-10-12 23:25:47 +0700

    20231012_1625_UTC

--- sqlite/problem_set_1/packages/log.sql
+++ sqlite/problem_set_1/packages/log.sql
@@ -1,28 +1,37 @@
 
 -- *** The Lost Letter ***
--- ????: At what type of address did the Lost Letter end up?
--- Get type address with datum is "900 Somerville Avenue" ---> result is: Residential
-SELECT type FROM addresses WHERE address LIKE '900 Somerville Avenue';
+-- I need to make sure there is an order related to 2 addresses: "900 Somerville Avenue" and "2 Finnigan Street" inside DB.
+-- I check ID address and result:
+	-- "900 Somerville Avenue" ---> result is 432
+	SELECT id FROM addresses WHERE address IS '900 Somerville Avenue';
+	-- "2 Finnigan Street" ---> result is 854
+	SELECT id FROM addresses WHERE address IS '2 Finnigan Street';
+-- And I have a complete query after trying the cases to determine which is the sending address and which is the receiving address. And more, that is "ID package"
+SELECT * FROM packages WHERE (from_address_id = 432 AND to_address_id = 854);
 
--- ????: At what address did the Lost Letter end up?
--- Get ID of package with datum is "900 Somerville Avenue" ---> result is: 432
-SELECT id FROM addresses WHERE address IS '900 Somerville Avenue';
--- After have ID package from query above (432), get receiver's address ID ---> result is: 3239
-SELECT to_address_id FROM packages WHERE id IS 432;
--- When the recipient's id is available (3239), get info from "addresses" table ---> result is: 30 Atlantic Avenue
-SELECT address FROM addresses WHERE id IS 3239;
--- Query with multiple related tables
+-- ????: At what type of address did the Lost Letter end up? ---> result is: Residential
+-- After have receiving address, I will check it to see "type address"  belongs to which type
+SELECT type FROM addresses WHERE id = (
+	SELECT to_address_id FROM packages WHERE id IS 384 
+);
+
+-- ????: At what address did the Lost Letter end up? ---> result is: 2 Finnigan Street
 SELECT address FROM addresses WHERE id = (
-	SELECT to_address_id FROM packages WHERE id = (
-		SELECT id FROM addresses WHERE address IS '900 Somerville Avenue'
-	)
+	SELECT to_address_id FROM packages WHERE id IS 384
 );
 .
 .
 .
 .
 -- *** The Devious Delivery ***
--- ????: At what type of address did the Devious Delivery end up?
+-- I don't know if I did it right or not.
+-- Or because check50 is not working correctly.
+-- But for the datum is "Afraid there’s no “From” address.".
+-- I queried the entire column "from_address_id"  from  "packages" table.
+-- And there is only one value "NULL".
+-- And from there I followed my intuition.
+
+-- ????: At what type of address did the Devious Delivery end up? ---> result is: Business
 -- With datum is: "Afraid there’s no “From” address". So we have "from_address_id" is NULL & we have query to find info package with "from_address_id" is NULL
 SELECT * FROM packages WHERE from_address_id ISNULL;
 -- After getting information from the above query, to get type address ---> result is: Business
@@ -32,17 +41,18 @@ SELECT type FROM addresses WHERE id IS (
 	SELECT id FROM packages WHERE from_address_id ISNULL
 );
 
--- ????: What were the contents of the Devious Delivery?
-We already have some information in the query above (SELECT * FROM packages WHERE from_address_id ISNULL;) including CONTENT information ---> result is: Duck debugger
+-- ????: What were the contents of the Devious Delivery? ---> result is: Duck debugger
+-- We already have some information in the query above 
+SELECT contents FROM packages WHERE from_address_id ISNULL;
 .
 .
 .
 .
 -- *** The Forgotten Gift ***
--- ????: What are the contents of the Forgotten Gift?
+-- ????: What are the contents of the Forgotten Gift? ---> result is: Flowers
 -- With datum from paragraph, we have ID address of sender & receiver respectively is (109 Tileston Street | 9873) & (728 Maple Place | 4983)
 SELECT id FROM addresses WHERE address IN ('728 Maple Place', '109 Tileston Street');
--- I will take the 2 IDs above to find CONTENT information ---> result is: Flowers
+-- I will take the 2 IDs above to find CONTENT information
 SELECT contents FROM packages WHERE (from_address_id = 9873 AND to_address_id = 4983);
 -- Query with multiple related tables
 SELECT p.contents
@@ -51,7 +61,7 @@ JOIN packages AS p ON a.id = p.from_address_id
 WHERE a.address
 IN ('728 Maple Place', '109 Tileston Street') AND p.to_address_id = 4983;
 
--- ????: Who has the Forgotten Gift?
+-- ????: Who has the Forgotten Gift? ---> result is: Mikel
 -- I need get package ID
 SELECT id FROM packages WHERE (from_address_id = 9873 AND to_address_id = 4983);
 -- I will look for driver ID with datum is package ID
@@ -59,4 +69,8 @@ SELECT driver_id FROM scans WHERE id = 9523;
 -- Once I have the driver id, I will find the driver's name
 SELECT name FROM drivers WHERE id = 17;
 -- Query with multiple related tables
-
+SELECT name FROM drivers WHERE id = (
+	SELECT driver_id FROM scans WHERE id = (
+		SELECT id FROM packages WHERE (from_address_id = 9873 AND to_address_id = 4983)
+	)
+);
