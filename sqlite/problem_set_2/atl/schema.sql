cc3f04173ad67764bbb0e8becb12fad1f62cc806
vnn1489 <vnn1489@duck.com>
2023-10-20 07:00:11 +0700

    20231020_0000_UTC

--- sqlite/problem_set_2/atl/schema.sql
+++ sqlite/problem_set_2/atl/schema.sql
@@ -1,24 +1,32 @@
 CREATE TABLE "passengers" (
-    "id" INTEGER,
+    "id" INTEGER PRIMARY KEY,
     "first_name" TEXT,
     "last_name" TEXT,
-    "age" INTEGER,
-    PRIMARY KEY("id")
+    "age" INTEGER
 );
 
-CREATE TABLE "check_in" (
-    "id" INTEGER,
+CREATE TABLE "check_ins" (
+    "id" INTEGER PRIMARY KEY,
     "date_time" NUMERIC NOT NULL,
+    "passenger_id" INTEGER,
+    "flight_id" INTEGER,
     FOREIGN KEY("passenger_id") REFERENCES "passengers"("id"),
-    PRIMARY KEY("id")
-);????
+    FOREIGN KEY("flight_id") REFERENCES "flights"("id")
+);
+
+CREATE TABLE "airlines" (
+    "id" INTEGER PRIMARY KEY,
+    "name" TEXT,
+    "concourse" TEXT CHECK("concourse" IN ('A', 'B', 'C', 'D', 'E', 'F', 'T'))
+);
 
 CREATE TABLE "flights" (
-    "id" INTEGER,
+    "id" INTEGER PRIMARY KEY,
     "flight_no" INTEGER NOT NULL,
-    "operating" TEXT,
-
-
-
-    PRIMARY KEY("id")
+    "from_code" TEXT,
+    "to_code" TEXT,
+    "from_time" TEXT,
+    "to_time" TEXT,
+    "airline_id" INTEGER,
+    FOREIGN KEY("airline_id") REFERENCES "airlines"("id")
 );
