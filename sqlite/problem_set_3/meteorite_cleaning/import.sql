a243db791ab2efa56b02fe8aa3beecb03492b347
vnn1489 <vnn1489@outlook.com>
2023-10-23 21:19:05 +0700

    20231023_1419_UTC

---
+++ sqlite/problem_set_3/meteorite_cleaning/import.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE "meteorites" (
+--     "id" INTEGER PRIMARY KEY,
+--     "name" TEXT,
+--     "type" TEXT,
+--     "class" TEXT,
+--     "mass" REAL,
+--     "discovery" TEXT,
+--     "year" REAL,
+--     "lat" REAL,
+--     "long" REAL
+-- );
+
+-- .import './meteorites.csv' meteorites;
+
+-- CREATE TABLE IF NOT EXISTS "temp"(
+
+-- )
+
+
