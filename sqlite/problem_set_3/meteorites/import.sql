eda498c6fe2e0e90afb3e8e55200dc9e7713b37b
vnn1489 <vnn1489@outlook.com>
2023-10-24 20:16:14 +0700

    20231024_1316_UTC

--- sqlite/problem_set_3/meteorites/import.sql
+++ sqlite/problem_set_3/meteorites/import.sql
@@ -5,7 +5,7 @@ CREATE TABLE "meteorites_temp" (
   "class" TEXT,
   "mass" REAL,
   "discovery" TEXT,
-  "year" REAL,
+  "year" INTEGER,
   "lat" REAL,
   "long" REAL
 );
@@ -13,26 +13,27 @@ CREATE TABLE "meteorites_temp" (
 .import --csv --skip 1 meteorites.csv meteorites_temp
 
 CREATE TABLE "meteorites" (
-  "id" INTEGER PRIMARY KEY,
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "name" TEXT,
   "class" TEXT,
   "mass" REAL,
   "discovery" TEXT CHECK("discovery" IN ('Fell', 'Found')),
-  "year" REAL,
+  "year" INTEGER,
   "lat" REAL,
   "long" REAL
 );
 
-INSERT INTO "meteorites" ("id", "name", "class", "mass", "discovery", "year", "lat", "long")
-SELECT  "id",
-        "name",
+DELETE FROM "meteorites_temp" WHERE "nametype" IS 'Relict';
+
+INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
+SELECT  "name",
         "class",
+        CASE WHEN "mass" = '' THEN NULL ELSE ROUND("mass", 2) END,
         "discovery",
         CASE WHEN "year" = '' THEN NULL ELSE "year" END,
-        CASE WHEN "mass" = '' THEN NULL ELSE "mass" END,
-        CASE WHEN "lat" = '' THEN NULL ELSE "lat" END,
-        CASE WHEN "long" = '' THEN NULL ELSE "long" END
-FROM "meteorites_temp";
+        CASE WHEN "lat" = '' THEN NULL ELSE ROUND("lat", 2) END,
+        CASE WHEN "long" = '' THEN NULL ELSE ROUND("long", 2) END
+FROM "meteorites_temp"
+ORDER BY COALESCE("year", '') ASC, "name" ASC;
 
 DROP TABLE "meteorites_temp";
-
