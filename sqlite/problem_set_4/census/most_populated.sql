c95d8ae2088b59c249ff8873b2b4b361a1898f9d
vnn1489 <vnn1489@duck.com>
2023-10-26 07:19:21 +0700

    20231026_0019_UTC

---
+++ sqlite/problem_set_4/census/most_populated.sql
@@ -0,0 +1,10 @@
+CREATE VIEW most_populated AS
+SELECT
+	district,
+	SUM(families) AS total_families,
+	SUM(households) AS total_households,
+	SUM(population) AS total_population,
+	SUM(male) AS total_male,
+	SUM(female) AS total_female 
+FROM census
+ORDER BY population DESC;
