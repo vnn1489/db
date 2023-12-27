9d2b3fa787668d8ca54692589f0e6685e80bed4d
vnn1489 <vnn1489@duck.com>
2023-12-27 21:42:31 +0700

    Wed 20231227 14:42 (24h) UTC

--- sqlite/4.viewing/bnb/available.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW available AS
-SELECT listings.id, listings.property_type, listings.host_name, availabilities.date
-FROM listings
-JOIN availabilities ON availabilities.listing_id = listings.id;
